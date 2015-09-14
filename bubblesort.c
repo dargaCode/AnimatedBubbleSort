ea4ce8118d052d6aac55fd9c1953a990598b46b6
dargaCode <darga.code@gmail.com>
2015-09-14 15:25:22 -0400

    further improved printouts to show progress

--- bubblesort.c
+++ bubblesort.c
@@ -6,7 +6,7 @@
 // prototypes
 void fill_array(int arr[], int len);
 void sort_array(int arr[], int len);
-void print_array(int arr[], int len);
+void print_data(int arr[], int len, int active);
 void delay_1_sec();
 
 int main(int argc, string argv[])
@@ -39,7 +39,8 @@ void fill_array(int arr[], int len)
     
     for (int i = 0; i < len; i++)
     {
-        arr[i] = rand() % (len * 4); // randomized
+        arr[i] = rand() % (len * 4); // randomized  
+        //todo change 4 to 1
         // arr[i] = len - i; // reverse order        
     }
 }
@@ -47,18 +48,45 @@ void fill_array(int arr[], int len)
 /*
  * Print out all the elements in the array
  */
-void print_array(int arr[], int len)
+void print_data(int arr[], int len, int active)
 {
-    printf("[");
-    for (int i = 0; i < len; i++)
+    // loop one higher than normal, to add ) after last
+    for (int i = 0; i <= len; i++)
     {
-        printf("%i", arr[i]);
-        if (i < len - 1)
+        int relative_pos = i - active;
+        switch (relative_pos)
         {
-            printf(",");
+            case 0:
+                printf("(");
+                break;
+            case 1:
+                // show the comparison
+                if (arr[active] < arr[active + 1])
+                {
+                    printf("<");
+                }
+                else if (arr[active] > arr[active + 1])
+                {
+                    printf(">");
+                }
+                else
+                {
+                    printf("=");
+                }
+                break;
+            case 2:
+                printf(")");
+                break;
+            default:
+                printf(" ");
+                break;
+        }
+        // leave off the last number
+        if (i < len)
+        {
+            printf("%i", arr[i]);
         }
     }
-    printf("]");
 }
 
 /*
@@ -67,7 +95,7 @@ void print_array(int arr[], int len)
 void delay_1_sec()
 {
     // delay test
-    int wake = time(0) + 0;
+    int wake = time(0) + 1;
     while (time(0) < wake)
     {
     }
@@ -79,31 +107,39 @@ void delay_1_sec()
 void sort_array(int arr[], int len)
 {   
     delay_1_sec();
-    print_array(arr, len);
-    printf("\n\n");
+    print_data(arr, len, -10);
+    printf("- START! \n");
+    delay_1_sec();
+    printf("\n");
     
     // optimize loop to get smaller each time
     for (int i = len - 1; i >= 0; i--)
-    {        
+    {      
         // compare each pair
         for (int j = 0; j < i; j++)
         {
-            //printf("compare [%i] and [%i] \n", j, j + 1);
             string status = "";
             delay_1_sec(); 
+            print_data(arr, len, j);
             // compare each number and swap if neccesary
             if (arr[j + 1] < arr[j])
             {
-                status = " - swap!";
+                status = "- SWAP!";
                 int temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
-            print_array(arr, len);
+
             printf("%s \n", status);
         }
+        if (i > 0)
+        {
+            delay_1_sec();
+            printf("\n");
+        }  
     }
     delay_1_sec();
-    printf("DONE! \n");
+    print_data(arr, len, -10);
+    printf("- DONE! \n");
     delay_1_sec();
 }
